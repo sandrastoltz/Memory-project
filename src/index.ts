0922c287a14d56e1d0e9ec6e5b3e7067fa9536d4
Sandra <sandra.stoltz@hotmail.com>
2023-12-12 14:12:17 +0100

    function to shuffle cards is working

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
 console.log('start'); 
 
-// Vid start ska korten slumpas ut, en math-random funktion
 // Göra korten klickbara
 // Få korten att vända vid klick
 // Begränsa att man bara kan vända två kort åt gången
 // Funktion för att jämföra de två kort som vändes
-// If-else sats, om de två korten är lika ska de ej vändas tillbaka och om de är olika ska de båda vändas tillbaka
+// If-else sats, om de två korten är lika ska de ej vändas tillbaka och om de är olika ska de   båda vändas tillbaka
 // När alla kort är vända ska pop-up fönstret visas 'you won'
 // Göra x-knappen klickbar och resetta spelet, alltså vända tillbaka alla kort och slumpa ut dom på nytt
+
+// 'null' är efter HTMLElement, eftersom querySelectorn returnerar null om den inte hittar något element i '.memory-cards'
+let cardContainer: HTMLElement | null = document.querySelector('.memory-cards');
+let memoryCard: NodeListOf<HTMLElement> = document.querySelectorAll('.memory-card');
+
+function shuffleCards(): void {
+    // 'Array.from(cards)' konverterar NodeList i memoryCard till en array med alla kort inuti.
+    let cards: NodeListOf<Element> = document.querySelectorAll('.memory-card');
+    let cardArray: Element[] = Array.from(cards);
+
+    // Loopar igenom arrayen från slutet till början och blandar korten enligt Fisher-Yates shuffle metoden.
+    // Alltså för varje loop tas ett slumpat index mellan 0 och det nuvarande, sedan byts elementet vid det slumpade indexet ut med elementet vid det nyvarnade indexet.
+    for (let i = cardArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [cardArray[i], cardArray[j]] = [cardArray[j], cardArray[i]];
+    }
+
+    // Lägger alla blandade kort i cardContainer så de visas på sidan.
+    if (cardContainer) {
+        cardContainer.innerHTML = '';
+        cardArray.forEach(card => {
+            cardContainer.appendChild(card);
+        });
+    }
+}
+shuffleCards();
