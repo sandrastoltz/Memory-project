5a4c639e5f260384491aa673669752ede3db3a8c
Sandra <sandra.stoltz@hotmail.com>
2023-12-12 12:41:38 +0100

    psudokod

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,9 @@
 console.log('start');
-console.log('test');
+// Vid start ska korten slumpas ut, en math-random funktion
+// Göra korten klickbara
+// Få korten att vända vid klick
+// Begränsa att man bara kan vända två kort åt gången
+// Funktion för att jämföra de två kort som vändes
+// If-else sats, om de två korten är lika ska de ej vändas tillbaka och om de är olika ska de båda vändas tillbaka
+// När alla kort är vända ska pop-up fönstret visas 'you won'
+// Göra x-knappen klickbar och resetta spelet, alltså vända tillbaka alla kort och slumpa ut dom på nytt
